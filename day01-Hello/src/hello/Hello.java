f3d177605b37974d97ca0dcd2c390515ca310430
LeeKwonWoo <kenu12@Naver.com>
2019-07-04 11:13:08 +0900

    sysout 구문 사용법

--- day01-Hello/src/hello/Hello.java
+++ day01-Hello/src/hello/Hello.java
@@ -9,6 +9,9 @@ public class Hello {
 		// 자바는 대소문작 ㅏ틀리면 구문오류인 경우가
 		// 많으므로 대소문자 오타주의
 		System.out.println("Hello, World!");
+		// 자바 기본 출력 구문
+		// sysout -> ctrl + space bar
+		System.out.println("안녕하세요, 자바!");
 	}
 
 }
