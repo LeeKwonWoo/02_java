534d5b0bb5b6dab458aaa9e83bfaffd9b3456f08
LeeKwonWoo <kenu12@Naver.com>
2019-07-04 15:49:47 +0900

    boolean 타입사용
    boolean 타입테스트 변수선언,초기화, 사용

---
+++ day01-Hello/src/type/primitive/BooleanTest.java
@@ -0,0 +1,27 @@
+package type.primitive;
+
+/*
+ * 논리 타입인 boolean 
+ * 테스트하는 클래스
+ * 
+ */
+
+public class BooleanTest {
+
+	public static void main(String[] args) {
+		// 1. 선언
+		boolean isLikeBaseball;
+		boolean isExists;
+		boolean isHave;
+		boolean isEnd = false; // 선언과 동시에 초기화
+		// 2. 초기화
+		isLikeBaseball = true;
+		isExists = true;
+		isHave = false;
+		System.out.println("isLikeBaseball : " + isLikeBaseball);
+		System.out.println("isExists : " + isExists);
+		System.out.println("isHave : " + isHave);
+		System.out.println("isEnd : " + isEnd);
+	}
+
+}
