b96740db457e133d4ea5ee384474a05c4d89c994
LeeKwonWoo <kenu12@Naver.com>
2019-07-05 09:40:44 +0900

    charTest

---
+++ day02-Types/src/type/primitive/CharTest.java
@@ -0,0 +1,28 @@
+package type.primitive;
+
+/*
+ * char : 문자 1개를 저장하는 타입
+ * 내부적으로는 숫자로 저장하는 특징이 있다.
+ */
+public class CharTest {
+	public void main(String[] args) {
+		// 1. 선언 : 타입이름 변수이름;
+		char input;
+		// 2. 초기화 : 변수이름 = 값;
+		input = 'a';
+		// 3. 사용
+		System.out.println("input = " + input);
+		// 4. char 가 내부적으로 숫자로 저장되는 것 확인
+		// 정수를 저장하는 변수 code를 선언 하면서
+		// 문자를 저장하는 변수 input 의 값을 복사
+		int code = input;
+		System.out.println("code = " + code);
+		
+		// code 는 정수를 저장하는 변수 이므로
+		// + 1 하는 산술연산을 하여 값을 증가시키고
+		code += 1;
+		System.out.println("code = " + code);
+		System.out.println("(char)code = " + (char)code);
+		
+	}
+}
