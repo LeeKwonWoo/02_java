9e29d6a9db0a8c0f3c5d97f0305d895cdca044ba
LeeKwonWoo <kenu12@Naver.com>
2019-07-22 14:50:20 +0900

    named point 실습

--- day12-Inheritance/src/point/PointTest.java
+++ day12-Inheritance/src/point/PointTest.java
@@ -10,12 +10,13 @@ public class PointTest {
 		// 1. 변수선언
 		Point p;
 		Point q;
+		Point o;
 		Point[] points = new Point[2];
 		
 		// 2. 초기화
 		p = new Point(0,0);
 		q = new Point(3,4);
-		
+		o = new NamedPoint("O",0,0);
 		points[0] = new Point(0,0);
 		points[1] = new Point(3,4);
 		// 3. 사용
@@ -26,8 +27,13 @@ public class PointTest {
 		//(2) 점 q를 출력
 		System.out.println(q);
 		System.out.println(points[1]);
-		//(3) 두 점 사이의 거리를 계산
+		
+		//(3) 점 o를 출력
+		System.out.println(o);
+		//(4) 두 점 사이의 거리를 계산
 		System.out.printf("두 점사이의 거리 %5.2f%n",p.distanceFrom(q));
+		//(5) 이름이 붙은 원점과 점q 의 거리 계산
+		System.out.printf("원점과 점q의 거리 :%5.2f%n",o.distanceFrom(q));
 	}
 
 }
